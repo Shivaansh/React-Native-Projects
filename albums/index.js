18f4ae9993eb8f68dd94ad705441239111f7d82f
Shivaansh <shivaanshseal@gmail.com>
2018-10-16 16:01:56 -0400

    Created first project: Album

---
+++ albums/index.js
@@ -0,0 +1,14 @@
+/** @format */
+
+// import a library to help create a component
+import React from 'react';
+import ReactNative from 'react-native';
+
+//create component
+//components are intelligent objects
+//have lots of inner working associated
+
+
+//render component to device
+//need to tell React to take created component and place
+//it somewhere in app
