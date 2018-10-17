c28095c04c5f13a8fccf25dd9099853dcbfc2cc3
Shivaansh <shivaanshseal@gmail.com>
2018-10-17 16:13:50 -0400

    Text component added and rendered

--- albums/index.js
+++ albums/index.js
@@ -1,14 +1,22 @@
 /** @format */
 
 // import a library to help create a component
-import React from 'react';
-import ReactNative from 'react-native';
-
+import React from 'react'; //import React library
+import { Text, AppRegistry } from 'react-native'; 
+//import only Text property from React Native library
 //create component
-//components are intelligent objects
+//components are intelligent objects, create text/content on m. device screen
 //have lots of inner working associated
 
+const App = () => (//function, returns some JSX
+        <Text>Some Text</Text> //text to show (HTML)
+);
+//this is a JSX method, JSX follows HTML semantics
+//JSX tells react native about the component
 
 //render component to device
 //need to tell React to take created component and place
 //it somewhere in app
+
+AppRegistry.registerComponent('albums', () => App);
+//this method registers component App to project albums
