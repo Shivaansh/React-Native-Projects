31aa891f0f9f1a07bc118d724447f884a7c31630
Shivaansh <shivaanshseal@gmail.com>
2018-10-24 16:05:10 -0400

    Header component rendered

--- albums/index.js
+++ albums/index.js
@@ -1,16 +1,22 @@
 /** @format */
 
 // import a library to help create a component
+
 import React from 'react'; //import React library
-import { Text, AppRegistry } from 'react-native'; 
+import { AppRegistry } from 'react-native'; 
+import Header from './src/components/header'; //pathname for file containing header component
+
+//need to provide path for self-created js files
+
 //import only Text property from React Native library
 //create component
 //components are intelligent objects, create text/content on m. device screen
 //have lots of inner working associated
 
 const App = () => (//function, returns some JSX
-        <Text>Some Text</Text> //text to show (HTML)
+        <Header />
 );
+
 //this is a JSX method, JSX follows HTML semantics
 //JSX tells react native about the component
 
@@ -19,4 +25,7 @@ const App = () => (//function, returns some JSX
 //it somewhere in app
 
 AppRegistry.registerComponent('albums', () => App);
+
 //this method registers component App to project albums
+//ONLY CREATE ONE COMPONENT PER FILE
+//AppRegistry is only used by the Root component of the app
---
+++ albums/src/components/header.js
@@ -0,0 +1,16 @@
+//thjs file contains the JSK for the header component
+// of the app
+
+//import libraries
+import React from 'react';
+import { Text } from 'react-native';
+
+//define component
+//fn name = containing file name (rule of thumb)
+const Header = () => {
+    return <Text> THIS IS THE HEADER </Text>;
+};
+
+//expose component to app (NOT RENDER)
+export default Header;
+//makes Header component available to other files
